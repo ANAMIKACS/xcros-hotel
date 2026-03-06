ba78e7bc5bc3508270bec092f3ee7894f1cede23
ANAMIKACS <csanamika3@gmail.com>
2026-03-06 14:00:51 +0530

    new component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RoomBookingListing } from './room-booking-listing/room-booking-listing';
 import { ToursTravelsListing } from './tours-travels-listing/tours-travels-listing';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    {path:'', component:Home},
     {path:'roomBookingListing', component:RoomBookingListing},
     {path:'toursTravelsListing', component:ToursTravelsListing}
 ];
