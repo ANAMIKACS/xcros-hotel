6571ced689feadca2aa999346e8a21c6319e41a7
ANAMIKACS <csanamika3@gmail.com>
2026-03-08 20:03:10 +0530

    added animate.css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RoomBookingListing } from './room-booking-listing/room-booking-listing';
 import { ToursTravelsListing } from './tours-travels-listing/tours-travels-listing';
 import { Home } from './home/home';
+import { Hoteldiscount } from './hoteldiscount/hoteldiscount';
 
 export const routes: Routes = [
     {path:'', component:Home},
     {path:'roomBookingListing', component:RoomBookingListing},
-    {path:'toursTravelsListing', component:ToursTravelsListing}
+    {path:'toursTravelsListing', component:ToursTravelsListing},
+    {path:'hoteldiscount', component:Hoteldiscount}
 ];
